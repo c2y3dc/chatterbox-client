3b1fdee6db71af17e1eb67513d8e522d5d5956b6
c2y3dc <ervinchow@gmail.com>
2014-11-10 15:13:50 -0800

    app behavior-sending specs passed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,20 @@
 // YOUR CODE HERE:
-var app = {
-  init: function(){
+var app = {};
 
+app.init = function(){};
+app.send = function(message){
+  $.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
   }
+});
 };
-
