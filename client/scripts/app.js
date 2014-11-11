dae8966db298c6dfe0c11e57bbbad00882def681
c2y3dc <ervinchow@gmail.com>
2014-11-10 16:11:02 -0800

    prevented js injection & message display

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,58 @@
 // YOUR CODE HERE:
 var app = {};
-
+app.server = 'https://api.parse.com/1/classes/chatterbox';
 app.init = function(){};
+
+// message format:
+// var message = {
+//   'username': 'shawndrost',
+//   'text': 'trololo',
+//   'roomname': '4chan'
+// };
+
 app.send = function(message){
   $.ajax({
   // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
+  url: app.server,
   type: 'POST',
   data: JSON.stringify(message),
   contentType: 'application/json',
   success: function (data) {
-    console.log('chatterbox: Message sent');
+    console.log('chatterbox: Message sent');app.messageHandler(data);
   },
   error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
+    console.error('chatterbox: Failed to send message. Error data:', data);
   }
 });
 };
+
+app.fetch = function(){
+  $.ajax({
+  // always use this url
+  url: app.server,
+  type: 'GET',
+  // data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message received. contents:', data);
+    app.messageHandler(data);
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to get message');
+  }
+});
+}
+
+app.messageHandler = function(data){
+  var messages = data.results;
+  for(var i=0; i < messages.length; i++){
+    if(messages[i].text !== undefined && messages[i].text.slice(0,8) !== '<script>'){
+    // console.log(messages[i].username + ": " + messages[i].text)
+      $('.chat').append('<div>' + messages[i].username + ": " + messages[i].text + '</div>');
+    }
+  }
+}
+
+app.fetch();
